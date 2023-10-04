9ab84191d2c08e4935ce23e8e72ed18f0469e637
Jerry_TJU <1422768678@qq.com>
2023-10-04 20:03:14 +0800

    导航栏

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
 <template>
+  <header-nav></header-nav>
   <router-view></router-view>
 </template>
 
 <script setup>
-
+import headerNav from "@/components/header-nav.vue"
 </script>
+
+<style>
+body, html, #app{
+    width:100vw;
+    margin:0;
+}
+p{
+  margin: 0;
+}
+</style>
