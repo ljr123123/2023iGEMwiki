46cb9384b00434f7e9cfb355df6282471e110ef1
Pama <1327374483@qq.com>
2023-10-05 20:18:26 +0800

    [New] - 完善路由与初始化相应页面

--- src/router/index.js
+++ src/router/index.js
@@ -35,6 +35,98 @@ const routes = [
 		path: "/team",
 		component: () => import("@/views/team.vue"),
 	},
+	{
+		component: () => import("@/views/DescriptionView.vue"),
+		path: "/description",
+	},
+	{
+		component: () => import("@/views/design.vue"),
+		path: "/design",
+	},
+	{
+		component: () => import("@/views/engineering.vue"),
+		path: "/engineering",
+	},
+	{
+		component: () => import("@/views/experiments.vue"),
+		path: "/experiments",
+	},
+	{
+		component: () => import("@/views/results.vue"),
+		path: "/results",
+	},
+	{
+		component: () => import("@/views/implementation.vue"),
+		path: "/implementation",
+	},
+	{
+		component: () => import("@/views/ContributionView.vue"),
+		path: "/contribution",
+	},
+	{
+		component: () => import("@/views/safety.vue"),
+		path: "/safety",
+	},
+	{
+		component: () => import("@/views/NotebookView.vue"),
+		path: "/notebook",
+	},
+	{
+		path: "/model",
+		component: () => import("@/views/model.vue"),
+	},
+	{
+		path: "/software",
+		component: () => import("@/views/software.vue"),
+	},
+	{
+		path: "/hardware",
+		component: () => import("@/views/hardware.vue"),
+	},
+	{
+		path: "/human-overview",
+		component: () => import("@/views/human-overview.vue"),
+	},
+	{
+		path: "/human-practice",
+		component: () => import("@/views/human-practice.vue"),
+	},
+	{
+		path: "/communication",
+		component: () => import("@/views/communication.vue"),
+	},
+	{
+		path: "/game",
+		component: () => import("@/views/game.vue"),
+	},
+	{
+		path: "/team",
+		component: () => import("@/views/team.vue"),
+	},
+	{
+		path: "/attributions",
+		component: () => import("@/views/attributionsView.vue"),
+	},
+	{
+		path: "/collaborations",
+		component: () => import("@/views/collaborations.vue"),
+	},
+	{
+		path: "/gold",
+		component: () => import("@/views/gold.vue"),
+	},
+	{
+		path: "/silver",
+		component: () => import("@/views/silver.vue"),
+	},
+	{
+		path: "/bronze",
+		component: () => import("@/views/bronze.vue"),
+	},
+	{
+		path: "/awards",
+		component: () => import("@/views/awards.vue"),
+	},
 ];
 
 const router = createRouter({
