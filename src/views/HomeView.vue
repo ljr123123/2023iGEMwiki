ee4f0f228b3dfabdabe678ac67845ed0ea2138cd
Jerry_TJU <1422768678@qq.com>
2023-10-05 12:32:59 +0800

    新增首页顶部GIF

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,19 @@
 <template>
-  <div></div>
+  <div class="home-view">
+    <video src="@/assets/gif.webm" autoplay="true" class="home-top-image"/>
+  </div>
 </template>
 
 <script setup>
-
 </script>
+
+<style scoped>
+.home-view{
+  width:100vw;
+}
+.home-top-image{
+  width:100vw;
+  position: absolute;
+  top:15vh;
+}
+</style>
