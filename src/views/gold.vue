42427335e16910cb400aa87d13fef3dd4fc04d03
pamaforce <1327374483@qq.com>
2023-10-12 23:43:11 +0800

    [New] - 完成Gold页面

--- src/views/gold.vue
+++ src/views/gold.vue
@@ -6,12 +6,34 @@
     />
     <div class="page-title">Gold</div>
     <content-decorate style="margin-top: -20vh" :navigation="navigation">
+      <TextWithImage
+        mainTitle="Best Integrated Human Practices"
+        :textContent="textContent"
+      ></TextWithImage>
+      <TextWithImage
+        mainTitle="Best New Basic Part"
+        :textContent="textContent2"
+      ></TextWithImage>
+      <TextWithImage
+        mainTitle="Best New Improved Part"
+        :textContent="textContent3"
+      ></TextWithImage>
     </content-decorate>
   </div>
 </template>
   
   <script setup>
 import ContentDecorate from "@/components/content-decorate.vue";
+import TextWithImage from "@/components/text-with-image.vue";
+const textContent = [
+  `This year, through telephone interviews, email Q & A, on-site visits, street activities, volunteer teaching and other forms, we included people of all ages and different fields into our HP scope, and provided a lot of support for the project. See the <a href="https://2023.igem.wiki/tjusls-china/human-practices">Human Practice</a> page for more information.`,
+];
+const textContent2 = [
+  `We found that none of the previous iGEM involved enzymes which is capable of degrading PE. As a result, its sequences have not yet been translated into BioBrick parts. We thought this was cool and included two PE degrading enzymes in the Part! See the <a href="https://2023.igem.wiki/tjusls-china/parts">Parts</a> page for details.`,
+];
+const textContent3 = [
+  `In order to improve the thermostability of PEase, we carried out rational design, and finally obtained 64 mutants. Of all the new parts, 25 have improved functionality compared to the original parts. The best mutant showed a greater than 12% increase in thermostability than the wild type. See the <a href="https://2023.igem.wiki/tjusls-china/parts">Parts</a> page for more information. `,
+];
 const navigation = [];
 </script>
   
